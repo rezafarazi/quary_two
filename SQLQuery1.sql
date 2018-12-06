fd892b4b74baddb5f6c935f7c36d9416a21b5096
rezafarazi <45543047+rezafarazi@users.noreply.github.com>
2018-12-06 16:45:48 +0330

    Quary
    DataBase : SQL SERVER

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,10 +2,8 @@
 
 
 
-
-
 ---------Create Person Table-------
-CREATE Table Persen(id INT NOT NULL PRIMARY KEY IDENTITY,student_number nchar(15),major_id int);
+CREATE Table Persen(id INT NOT NULL PRIMARY KEY IDENTITY,student_number nchar(15),major_id int,CONSTRAINT [FK_Student_ToTable] FOREIGN KEY (major_id) REFERENCES [Major]([id]));
 
 
 ---------SELECT Person Table-------
@@ -14,7 +12,7 @@ SELECT * FROM dbo.Person;
 
 
 ---------Create Student Table-------
-CREATE TABLE [dbo].[Student] ([id] INT IDENTITY (1, 1) NOT NULL,[frist_name]  NVARCHAR (50) NULL,[last_name]   NVARCHAR (50) NULL,[Person_id]   INT           NULL,[national_id] NCHAR (10)    NULL,[age]         INT           NULL,PRIMARY KEY CLUSTERED ([id] ASC),CONSTRAINT [FK_Student_ToTable] FOREIGN KEY (Person_id) REFERENCES [Persen]([id]));
+CREATE TABLE Student ([id] INT IDENTITY (1, 1) NOT NULL,[frist_name]  NVARCHAR (50) NULL,[last_name]   NVARCHAR (50) NULL,[Person_id]   INT           NULL,[national_id] NCHAR (10)    NULL,[age]         INT           NULL,PRIMARY KEY CLUSTERED ([id] ASC),CONSTRAINT [FK_Student_ToTable] FOREIGN KEY (Person_id) REFERENCES [Persen]([id]));
 
 
 
@@ -40,7 +38,50 @@ SELECT * FROM Major;
 
 
 
+----------INSERT Data To Person--------
+INSERT INTO Persen (student_number,major_id) VALUES('99',2),('222',3),('58',4),('25',5),('58',6),('587',7),('457852766',8),('45666',9),('456646',10);
 
+
+
+----------INSERT Data To Student--------
+INSERT INTO Student (frist_name,last_name,Person_id,national_id,age) VALUES('xxx','ssss',2,'9090',29),('ahmadi','ali',3,'40',29),('erza','rezaei',4,'040',29),('xa','xei',5,'62',29),('xosro','xosravi',6,'1140',29),('ddddd','dddd',7,'4040',29),('ccc','cccc',8,'65',29),('ccc','cd',9,'2020',40);
+
+
+
+
+
+
+
+------------SELECT JOIN---------
+SELECT * FROM Student JOIN Persen ON Student.Person_id=Persen.id;
+
+
+
+
+------------SELECT CROSS JOIN---------
+SELECT * FROM Student CROSS JOIN Persen;
+
+
+
+------------SELECT LEFT JOIN---------
+SELECT * FROM Student LEFT OUTER JOIN Persen ON Student.Person_id=Persen.id;
+
+
+
+------------SELECT Right JOIN---------
+SELECT * FROM Student RIGHT OUTER JOIN Persen ON Student.Person_id=Persen.id;
+
+
+
+
+-----------WHERE----------
+SELECT * FROM Student RIGHT OUTER JOIN Persen ON Student.Person_id=Persen.id WHERE Student.id IS NOT NULL;
+
+
+
+
+-------SELECT 'a' Char In Between lastname----
+SELECT * FROM Student RIGHT OUTER JOIN Persen ON Student.Person_id=Persen.id WHERE Student.last_name LIKE '%a%';
 
 
 
