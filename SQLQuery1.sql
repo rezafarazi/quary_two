9675486386fc5353d8a0022b8e4bfab649a0492d
rezafarazi <45543047+rezafarazi@users.noreply.github.com>
2018-12-09 23:58:35 +0330

    database
    database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,87 +1,72 @@
-﻿------Reza Farazi-------
+﻿-----------Reza Farazi----------------
+
+-------return Table colnum count-------
+SELECT COUNT(*) FROM Student;
 
 
 
----------Create Person Table-------
-CREATE Table Persen(id INT NOT NULL PRIMARY KEY IDENTITY,student_number nchar(15),major_id int,CONSTRAINT [FK_Student_ToTable] FOREIGN KEY (major_id) REFERENCES [Major]([id]));
-
-
----------SELECT Person Table-------
-SELECT * FROM dbo.Person;
+-------return Table Avrage age -------
+SELECT AVG(age) FROM Student;
 
 
 
----------Create Student Table-------
-CREATE TABLE Student ([id] INT IDENTITY (1, 1) NOT NULL,[frist_name]  NVARCHAR (50) NULL,[last_name]   NVARCHAR (50) NULL,[Person_id]   INT           NULL,[national_id] NCHAR (10)    NULL,[age]         INT           NULL,PRIMARY KEY CLUSTERED ([id] ASC),CONSTRAINT [FK_Student_ToTable] FOREIGN KEY (Person_id) REFERENCES [Persen]([id]));
+-------return Table Minimum age -------
+SELECT min(age) FROM Student;
+
+
+
+-------return Table Maximum age -------
+SELECT MAX(age) FROM Student;
+
+
+
+-------return Table SUM age -------
+SELECT SUM(age) FROM Student;
 
 
 
 
----------Select Student Table-------
-SELECT * FROM student;
-
-
-
-
-
----------Create Major Table----------
-CREATE Table Major(id INT NOT NULL PRIMARY KEY IDENTITY,frist_name nvarchar(50),last_name nvarchar(50),national_id nchar(10),age INT);
-
-
-
----------Select Student Table-------
-SELECT * FROM Major;
+SELECT Count(Student.Person_id) FROM Student JOIN Persen ON Student.Person_id=Persen.id
 
 
 
 
 
 
-
-----------INSERT Data To Person--------
-INSERT INTO Persen (student_number,major_id) VALUES('99',2),('222',3),('58',4),('25',5),('58',6),('587',7),('457852766',8),('45666',9),('456646',10);
-
+--------CREATE A View---------
+CREATE VIEW rview AS SELECT * FROM Student
 
 
-----------INSERT Data To Student--------
-INSERT INTO Student (frist_name,last_name,Person_id,national_id,age) VALUES('xxx','ssss',2,'9090',29),('ahmadi','ali',3,'40',29),('erza','rezaei',4,'040',29),('xa','xei',5,'62',29),('xosro','xosravi',6,'1140',29),('ddddd','dddd',7,'4040',29),('ccc','cccc',8,'65',29),('ccc','cd',9,'2020',40);
 
+-----------USE A View-------------
+SELECT * FROM rview
 
 
 
 
-
-
-------------SELECT JOIN---------
-SELECT * FROM Student JOIN Persen ON Student.Person_id=Persen.id;
-
-
-
-
-------------SELECT CROSS JOIN---------
-SELECT * FROM Student CROSS JOIN Persen;
-
-
-
-------------SELECT LEFT JOIN---------
-SELECT * FROM Student LEFT OUTER JOIN Persen ON Student.Person_id=Persen.id;
-
-
-
-------------SELECT Right JOIN---------
-SELECT * FROM Student RIGHT OUTER JOIN Persen ON Student.Person_id=Persen.id;
+-------------USE Where-----------
+SELECT * FROM rview  WHERE rview.id<5
 
 
 
 
------------WHERE----------
-SELECT * FROM Student RIGHT OUTER JOIN Persen ON Student.Person_id=Persen.id WHERE Student.id IS NOT NULL;
+
+SELECT Student.frist_name AS Name,Persen.major_id AS MAJOR_ID FROM Student JOIN Persen ON Student.Person_id=Persen.id
 
 
 
 
--------SELECT 'a' Char In Between lastname----
-SELECT * FROM Student RIGHT OUTER JOIN Persen ON Student.Person_id=Persen.id WHERE Student.last_name LIKE '%a%';
+
+
+
+
+
+SELECT * FROM Student JOIN Persen ON Student.Person_id=Persen.id
+
+SELECT * FROM Student
+
+
+
 
 
 
